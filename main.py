dfc3a9ba63e7d4f830c31b922c6fe5cfb8e3fb6b
dung.cda <dung.cda@teko.vn>
2024-07-19 13:57:01 +0700

    fix cors

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ from api import api_router
 from config import config
 
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# Configure CORS to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 root_router = APIRouter()
 root_router.include_router(api_router, prefix="/api")
 app.include_router(root_router)
